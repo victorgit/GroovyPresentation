e17bfdadd87f6c251e456771b14413973f1e6f86
Victor Trakhtenberg <victortr75@gmail.com>
2015-01-19 01:08:11 +0200

    analytics

--- src/cs/analytics.groovy
+++ src/cs/analytics.groovy
@@ -1,5 +1,8 @@
 package cs
 
+@groovy.lang.Grab (group='joda-time', module='joda-time', version = '2.1')
+import org.joda.time.*
+
 class ApiQuery {
     String eventName
     String queryType
@@ -33,3 +36,31 @@ Map<String, List<KeyValueResult>> mapResultsByQueryIdJava(List<ApiResult> result
 def mapResultsByQueryIdGroovy(List<ApiResult> results)  {
     results.collectEntries { [it.query.queryId(), it.values] }
 }
+
+
+class Variant {
+    long id;
+}
+
+List<Long> getVariantIdsJava(List<Variant> variants) {
+    List<Long> ids = new ArrayList<>();
+    for (Variant variant : variants) {
+        ids.add(variant.id)
+    }
+    return ids;
+}
+
+def  getVariantIdsGroovy(List<Variant> variants) {
+    variants.collect(it.id)
+}
+
+def padTime(time) {
+    time.toString().padLeft(2, '0')
+}
+
+def secondsToTime(seconds) {
+    def period = new Period(seconds*1000)
+    "${padTime(period.getHours())}:${padTime(period.getMinutes())}:${padTime(period.getSeconds())}"
+}
+
+println (secondsToTime(301))
