402888224d3e8f83162809a71722bd90d0e61d78
Victor Trakhtenberg <victortr75@gmail.com>
2015-01-24 11:39:28 +0200

    interpolation

--- src/strings/Interpolation.groovy
+++ src/strings/Interpolation.groovy
@@ -1,13 +1,19 @@
 package strings
 
-class LanguageQuery {
-    def whichLanguage() {
-        "Groovy"
-    }
-}
+tableName = "EVENTS"
+columns = "id, name, date, value"
+bucketName = "my_bucket"
+fileNameOnS3 = "my_file"
+accessKey = "key"
+accessSecret = "secret"
+gzip = true
 
-def oldLanguage = "Java"
+command = "copy "+tableName+" ("+columns+") from 's3://"+bucketName+"/"+fileNameOnS3+"'" + " CREDENTIALS aws_access_key_id="+accessKey+";aws_secret_access_key="+accessSecret+" gzip "+gzip
+println command
 
-println "Every $oldLanguage developer must know ${new LanguageQuery().whichLanguage()}!"
+command = String.format("copy %s (%s) from 's3://%s/%s' CREDENTIALS aws_access_key_id=%s;aws_secret_access_key=%s gzip %s",
+        tableName, columns, bucketName, fileNameOnS3, accessKey, accessSecret, gzip)
+println command
 
-
+command = "copy $tableName ($columns) from 's3://$bucketName/$fileNameOnS3' CREDENTIALS aws_access_key_id=$accessKey;aws_secret_access_key=$accessSecret gzip $gzip"
+println command
