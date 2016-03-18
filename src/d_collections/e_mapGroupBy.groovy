05d65e6c4bf85be7e957013020f51087b6e4e594
Victor Trakhtenberg <victortr75@gmail.com>
2016-03-18 23:22:54 +0200

    more collections examples

--- src/d_collections/e_mapGroupBy.groovy
+++ src/d_collections/e_mapGroupBy.groovy
@@ -1,29 +1,26 @@
 package d_collections
 
-def BIRTHDAY_FORMAT = "yyyy-MM-dd"
-
-
     class User {
             String name
             String city
             Date birthday
             public String toString() { name }
-        }
+    }
 
-    def users = [
+     users = [
             new User(name: "mrhaki", city: "Tilburg",
-                    birthday: Date.parse(BIRTHDAY_FORMAT, "1973-09-07")),
+                    birthday: Date.parse("yyyy-MM-dd", "1973-09-07")),
             new User(name: "bob", city: "New York",
-                    birthday: Date.parse(BIRTHDAY_FORMAT, "1963-09-07")),
+                    birthday: Date.parse("yyyy-MM-dd", "1963-09-07")),
             new User(name: "britt", city: "Amsterdam",
-                    birthday: Date.parse(BIRTHDAY_FORMAT, "1973-12-07")),
+                    birthday: Date.parse("yyyy-MM-dd", "1973-12-07")),
             new User(name: "kim", city: "Amsterdam",
-                    birthday: Date.parse(BIRTHDAY_FORMAT, "1975-03-17")),
+                    birthday: Date.parse("yyyy-MM-dd", "1975-03-17")),
             new User(name: "liam", city: "Tilburg",
-                    birthday: Date.parse(BIRTHDAY_FORMAT, "1973-09-05"))
+                    birthday: Date.parse("yyyy-MM-dd", "1973-09-05"))
     ]
 
-    println ""
-    println users.groupBy { it.city }
-println ""
-    println users.groupBy({it.city}, {it.birthday.format('MMM')})
+    users.groupBy { it.city }
+    users.groupBy ( {it.city}, {it.birthday.format('MMM')} )
+
+
